b0d4f9b1d3d0265003eb90aa37d2c05486515c6c
Marek <marek.karwowski@johnlewis.co.uk>
2018-05-12 17:00:14 +0100

    Added phone number list

--- api/contacts.js
+++ api/contacts.js
@@ -1,7 +1,7 @@
 const contacts = [
-	{id: "c1", firstName: "John", lastName: "Doe", phoneNumbers: []},
+	{id: "c1", firstName: "John", lastName: "Doe", phoneNumbers: ['1231-123-123', '1233-123-123']},
 	{id: "c2", firstName: "Tom", lastName: "Black", phoneNumbers: []},
-	{id: "c3", firstName: "Tom", lastName: "Brown", phoneNumbers: []},
+	{id: "c3", firstName: "Tom", lastName: "Brown", phoneNumbers: ['123123-00999-999']},
 ];
 
 export function getContacts () {
--- components/ContactDetails.jsx
+++ components/ContactDetails.jsx
@@ -1,7 +1,20 @@
 import ContactListItem from './ContactListItem';
+import './ContactDetails.scss';
 
-export default (contact) => (
-	<div className="card contact-details" key={contact.id}>
-		<ContactListItem {...contact}/>
-	</div>
-);
+export default (contact) => {
+
+	let phoneNumbers = contact.phoneNumbers.map(phone => (
+		<li key={phone}>{phone}</li>
+	));
+
+	if (phoneNumbers.length > 0) {
+		phoneNumbers = (<ul className="phones">{phoneNumbers}</ul>)
+	}
+
+	return (
+		<div className="card contact-details" key={contact.id}>
+			<ContactListItem {...contact}/>
+			{phoneNumbers}
+		</div>
+	);
+}
